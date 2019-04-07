2a91dc01968b1a7f41052c709978ee56acba7c0f
NganBui <nganbui180995@gmail.com>
2019-04-07 22:05:19 +0700

    NganBui: Update Manifet

--- NotyficationMsg/app/src/main/java/com/example/project/firebase/MyFireBaseMessagingService.java
+++ NotyficationMsg/app/src/main/java/com/example/project/firebase/MyFireBaseMessagingService.java
@@ -8,9 +8,17 @@ import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.example.project.model.InfoMsg;
 import com.example.project.notyficationmsg.MainActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MyFireBaseMessagingService extends FirebaseMessagingService {
     @Override
@@ -20,10 +28,12 @@ public class MyFireBaseMessagingService extends FirebaseMessagingService {
         {
             Log.i("Notification title :",remoteMessage.getNotification().getTitle());
             showNotyfication(remoteMessage.getNotification().getTitle());
+            pareJson(remoteMessage.getNotification().getBody());
         }
         showNotyfication(remoteMessage.getData().get("body"), remoteMessage.getData().get("message"));
 
     }
+
 
     private void showNotyfication(String body) {
         showNotyfication(body,"google");
@@ -43,5 +53,52 @@ public class MyFireBaseMessagingService extends FirebaseMessagingService {
                 .setContentIntent(pendingIntent);
         NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         manager.notify(0, builder.build());
+    }
+
+    private void pareJson(String body) {
+        if (body != null) {
+            try {
+                JSONObject jsonObj = new JSONObject(body);
+
+                String nameProfessor = jsonObj.getString("professor");
+                String nameUniversity = jsonObj.getString("university");
+                String laboratory = jsonObj.getString("laboratory");
+                String salary = jsonObj.getString("salary");
+                String major = jsonObj.getString("major");
+                String languge = jsonObj.getString("languge");
+                String programmingSkills = jsonObj.getString("programming skills");
+                String dateline = jsonObj.getString("Dateline");
+
+                JSONObject contactObj = new JSONObject("contact");
+                String phone = contactObj.getString("phone");
+                String email = contactObj.getString("email");
+
+                InfoMsg infoMsg = new InfoMsg();
+
+                infoMsg.setNameProfessor(nameProfessor);
+                infoMsg.setNameUniversity(nameUniversity);
+                infoMsg.setLaboratory(laboratory);
+                infoMsg.setSalary(salary);
+                infoMsg.setMajor(major);
+                infoMsg.setLanguge(languge);
+                infoMsg.setProgrammingSkills(programmingSkills);
+                infoMsg.setPhone(Integer.parseInt(phone));
+                infoMsg.setEmail(email);
+                SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
+                try {
+
+                    Date date = formatter.parse(dateline);
+                    infoMsg.setDateline(date);
+
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
