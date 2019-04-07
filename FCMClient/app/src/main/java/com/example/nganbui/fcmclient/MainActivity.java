921d867e7fd037bd174b2ae597a86b1c7ee474b8
Ha Phan <phanthihongha17@gmail.com>
2019-04-07 20:08:37 +0700

    Ha Phan: Add android application

---
+++ FCMClient/app/src/main/java/com/example/nganbui/fcmclient/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.nganbui.fcmclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+        FirebaseMessaging.getInstance().subscribeToTopic("global");
+        String token = FirebaseInstanceId.getInstance().getToken();
+        new FireBaseITDask().execute(token);
+    }
+}
---
+++ FCMClient/app/src/main/java/com/example/nganbui/fcmclient/MyFireBaseIDService.java
@@ -0,0 +1,25 @@
+package com.example.nganbui.fcmclient;
+
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MyFireBaseIDService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+        FirebaseApp.initializeApp(this);
+        FirebaseMessaging.getInstance().subscribeToTopic("global");
+        final String token = FirebaseInstanceId.getInstance().getToken();
+        Toast.makeText(MyFireBaseIDService.this, token,Toast.LENGTH_LONG).show();
+        saveTokenToDB(token);
+    }
+
+    private void saveTokenToDB(String token) {
+        new FireBaseITDask().execute(token);
+    }
+}
