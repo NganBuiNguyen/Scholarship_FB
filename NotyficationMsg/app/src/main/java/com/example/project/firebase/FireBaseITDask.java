c337ce916ef480eb61a7db99fd44f95df9a34f93
Ha Phan <phanthihongha17@gmail.com>
2019-04-08 08:08:42 +0700

    Ha Phan save token device one time

--- NotyficationMsg/app/src/main/java/com/example/project/firebase/FireBaseITDask.java
+++ NotyficationMsg/app/src/main/java/com/example/project/firebase/FireBaseITDask.java
@@ -1,40 +1,64 @@
 package com.example.project.firebase;
 
+import android.net.SSLCertificateSocketFactory;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.apache.http.conn.ssl.AllowAllHostnameVerifier;
+
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class FireBaseITDask extends AsyncTask<String, Void, Boolean> {
     @Override
     protected Boolean doInBackground(String... strings) {
-        try{
-            Log.i("Token device :  ", strings[0]);
-            URL url = new URL("https://192.168.1.99:8090/saveToken/token=" + strings[0]);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        try {
+        if(strings[0] !=null){
+            URL url = new URL("https://192.168.0.9:8090/saveToken");
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-            InputStream inputStream = connection.getInputStream();
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            StringBuilder builder = new StringBuilder();
-            String line = bufferedReader.readLine();
-            while (line!=null)
-            {
-                builder.append(line);
-                line = bufferedReader.readLine();
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+            connection.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+            if (connection instanceof HttpsURLConnection) {
+                HttpsURLConnection httpsConn = connection;
+                httpsConn.setSSLSocketFactory(SSLCertificateSocketFactory.getInsecure(0, null));
+                httpsConn.setHostnameVerifier(new AllowAllHostnameVerifier());
+                String urlParameters = "token=" + strings[0];
+
+                // Send post request
+                httpsConn.setDoOutput(true);
+                DataOutputStream wr = new DataOutputStream(httpsConn.getOutputStream());
+                wr.writeBytes(urlParameters);
+                wr.flush();
+                wr.close();
+
+                InputStream inputStream = httpsConn.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                StringBuilder builder = new StringBuilder();
+
+                String line = bufferedReader.readLine();
+                while (line != null) {
+                    builder.append(line);
+                    line = bufferedReader.readLine();
+                }
+
+                boolean kt = builder.toString().contains("true");
             }
-            boolean kt = builder.toString().contains("true");
         }
-        catch (Exception ex)
-        {
-            Log.e("ERROR: ", ex.toString());
-        }
-        return null;
+            } catch(Exception ex){
+                Log.e("ERROR: ", ex.toString());
+            }
+
+            return null;
+
     }
 
     @Override
